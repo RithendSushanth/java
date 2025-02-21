4de02c9fd6b829ab7f511b3502a8c4a2f566d01a
mca24062 <mca24062@siescoms.in>
2025-02-21 16:09:16 +0530

    added changed line

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,6 +4,7 @@ class Fibonacci {
     int n = 100, firstTerm = 0, secondTerm = 1;
         
     System.out.println("Fibonacci Series Upto " + n + ": ");
+    System.out.println("Changed");
     
     while (firstTerm <= n) {
       System.out.print(firstTerm + ", ");
